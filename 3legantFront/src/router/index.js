16587b14341a1e970e7e50bfabb6520f13f0f7c9
tallaloic <tallaloicrossel@gmail.com>
2025-05-09 16:26:11 +0200

    commit test

--- 3legantFront/src/router/index.js
+++ 3legantFront/src/router/index.js
@@ -25,9 +25,7 @@ const router = createRouter({
     {
       path: '/shop',
       name: 'shop',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
+
       component: () => import('../views/ShopView.vue'),
     },
     {
